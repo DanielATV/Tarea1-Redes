687a9b16363ad092397be9253894075dadb4a6ef
Daniel Toro <daniel.torov@sansano.usm.cl>
2020-05-01 19:42:55 -0400

    consultads consecutivas

--- cliente.py
+++ cliente.py
@@ -1,40 +1,48 @@
 import socket as skt
 direccionServidor = "localhost"
-puertoServidor = 61335
+puertoServidor = 60339
 
 socketCliente = skt.socket(skt.AF_INET, skt.SOCK_STREAM)
 
 socketCliente.connect((direccionServidor, puertoServidor))
 
-send = "webcode.me"
+while True:
+
+    send = input("URL: ")
+
+    if send == "terminate":
+        socketCliente.send(send.encode())
+        socketCliente.close()
+        break
+        
+
+    
+    socketCliente.send(send.encode())
+    print("Envie URL")
+
+    puertoUDP = socketCliente.recv(2048).decode()
+    puertoUDP = int(puertoUDP)
+    print(puertoUDP)
 
 
 
-socketCliente.send(send.encode())
+    socketUDP = skt.socket(skt.AF_INET,skt.SOCK_DGRAM)
 
-puertoUDP = socketCliente.recv(2048).decode()
-puertoUDP = int(puertoUDP)
-print(puertoUDP)
+    estado= "OK"
+    print(estado)
 
+    socketUDP.sendto(estado.encode(),(direccionServidor,puertoUDP))
+    print("Envie estado")
 
-
-socketUDP = skt.socket(skt.AF_INET,skt.SOCK_DGRAM)
-
-estado= "OK"
-print(estado)
-
-socketUDP.sendto(estado.encode(),(direccionServidor,puertoUDP))
-print("Envie estado")
-
-ulrHeader,_ = socketUDP.recvfrom(2048)
+    ulrHeader,_ = socketUDP.recvfrom(2048)
 
 
-#print(ulrHeader.decode())
-socketUDP.close()
+    #print(ulrHeader.decode())
+    socketUDP.close()
 
-archivo = open("URL.txt", 'a')
-archivo.write(send + '\n')
-archivo.write(ulrHeader.decode())
-archivo.write("\n\n")
+    archivo = open("URL.txt", 'a')
+    archivo.write(send + '\n')
+    archivo.write(ulrHeader.decode())
+    archivo.write("\n\n")
 archivo.close()
-socketCliente.close()
+    
--- servidor.py
+++ servidor.py
@@ -1,7 +1,7 @@
 import socket as skt
 
-puertoServidor = 61334
-puertoUDP = 60238
+puertoServidor = 60339
+puertoUDP = 60239
 
 
 socketServidor = skt.socket(skt.AF_INET, skt.SOCK_STREAM)
@@ -9,72 +9,85 @@ socketServidor.bind(('', puertoServidor))
 
 socketServidor.listen(1)
 
+
+socketCliente, direccionCliente = socketServidor.accept()
+
 while True:
 
-    socketCliente, direccionCliente = socketServidor.accept()
+    print("Empieza el while")
+
+
+   
+    
     mensaje = socketCliente.recv(2048).decode()
+    print("Recivio mensaje")
 
-
-
-
-    socketURL = skt.socket(skt.AF_INET, skt.SOCK_STREAM)
-
-    socketURL.connect((mensaje,80))
-
-    request = "GET / HTTP/1.1\r\nHost: " + mensaje + "\r\n\r\n"
-
-
-    socketURL.sendall(request.encode())
-
-    data = socketURL.recv(1024)
-
-    respuesta = data.decode()
-
-    n = 0
-
-    for char in respuesta:
-        if char == '\r' and respuesta[n+1] == '\n' and respuesta[n+2] == '\r':
-            endHeader= n+2
-            break
-            
-            
-
-        n = n+1
-
-    headerUrl = respuesta[:n]
-
-    #mandarPuerto = "OK_"+ str(puertoUDP)
-
-    msg2= str(puertoUDP)
-
-
-    
-    #lista = respuesta.split("\r\n")
-    
-    #print(lista)
-
-
-    #Socket UDP
-    socketUDP = skt.socket(skt.AF_INET,skt.SOCK_DGRAM)
-    socketUDP.bind(('',puertoUDP))
-
-    socketCliente.send(msg2.encode())
-
-    mensajeUDP, asd = socketUDP.recvfrom(2048)
-    print(mensajeUDP)
-
+    if mensaje == "terminate":
+        socketCliente.close()
     
 
-    if mensajeUDP.decode() == "OK":
-        print("Entre")
-        socketUDP.sendto(headerUrl.encode(),asd)
-        print("Envie")
+    else:
+
+        print("Else")
+
+        socketURL = skt.socket(skt.AF_INET, skt.SOCK_STREAM)
+
+        socketURL.connect((mensaje,80))
+
+        request = "GET / HTTP/1.1\r\nHost: " + mensaje + "\r\n\r\n"
+
+
+        socketURL.sendall(request.encode())
+
+        data = socketURL.recv(1024)
+
+        respuesta = data.decode()
+
+        n = 0
+
+        for char in respuesta:
+            if char == '\r' and respuesta[n+1] == '\n' and respuesta[n+2] == '\r':
+                endHeader= n+2
+                break
+                
+                
+
+            n = n+1
+
+        headerUrl = respuesta[:n]
+
+        #mandarPuerto = "OK_"+ str(puertoUDP)
+
+        msg2= str(puertoUDP)
+
+
+        
+        #lista = respuesta.split("\r\n")
+        
+        #print(lista)
+
+
+        #Socket UDP
+        socketUDP = skt.socket(skt.AF_INET,skt.SOCK_DGRAM)
+        socketUDP.bind(('',puertoUDP))
+
+        socketCliente.send(msg2.encode())
+
+        mensajeUDP, asd = socketUDP.recvfrom(2048)
+        print(mensajeUDP)
+
         
 
-    socketUDP.close()
-    
+        if mensajeUDP.decode() == "OK":
+            print("Entre")
+            socketUDP.sendto(headerUrl.encode(),asd)
+            print("Envie")
+            
+
+        socketUDP.close()
+        
 
     
 
 
-    socketCliente.close()
+    
